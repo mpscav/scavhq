9a2761b1ae4aee8cde168d2760eaff1a4235e1d8
Roberto Pedroso <roberto@rpedroso.com>
2014-03-07 19:34:53 -0600

    basic items api

---
+++ scavhq.py
@@ -0,0 +1,31 @@
+from flask import Flask, Response, request
+from pymongo import MongoClient
+from bson import json_util
+import json
+
+app = Flask(__name__)
+client = MongoClient('localhost', 27017)
+db = client['scavhq-dev']
+
+@app.route('/items', methods=['GET'])
+def items_get_all():
+    items_json = json.dumps(list(db.items.find()), default=json_util.default)
+    return Response(response=items_json, status=200, mimetype="application/json")
+
+@app.route('/items', methods=['POST'])
+def items_add():
+    item = request.get_json()
+    db.items.insert(item)
+    return Response(status=201)
+
+@app.route('/items/<num>', methods=['POST'])
+def item_edit(num):
+    result = db.items.update({'num': int(num)}, {"$set": {u'page': 2}}, upsert=False, multi=False)
+    status = 404 if result['updatedExisting'] is False else 200
+    return Response(status=status)
+
+@app.route('/items/<num>', methods=['DELETE'])
+def item_destroy(num):
+    result = db.items.remove({'num': int(num)})
+    status = 404 if result['n'] is 0 else 200
+    return Response(status=status)
---
+++ testing.py
@@ -0,0 +1,71 @@
+import scavhq, unittest, json
+from pymongo import MongoClient
+
+class ScavhqTestCase(unittest.TestCase):
+    @classmethod
+    def setUpClass(self):
+        scavhq.app.config['TESTING'] = True
+        self.app = scavhq.app.test_client()
+
+        self.client = MongoClient('localhost', 27017)
+        self.items = self.client['scavhq-dev'].items
+
+    def tearDown(self):
+        self.items.remove()
+
+    @classmethod
+    def tearDownClass(self):
+        self.client.disconnect()
+
+class TestItemAPI(ScavhqTestCase):
+    def test_get_index_none_exist(self):
+        response = self.app.get('/items')
+        assert json.loads(response.data) == []
+
+    def test_get_index_with_items(self):
+        self.items.insert({'num': 1, 'page': 1, 'text': 'Bring me Big Bertha!', 'value': '43 points'})
+        response = self.app.get('/items')
+        assert json.loads(response.data)[0]['num'] == 1
+
+    def test_add_item(self):
+        response = self.app.post('/items', data=json.dumps(dict(
+            num=1,
+            page=1,
+            text='Bring me Big Bertha!',
+            value = '43 points'
+        )), content_type="application/json")
+
+        assert response.status_code == 201
+        
+        item = self.items.find_one({'text': 'Bring me Big Bertha!'})
+        assert item['num'] == 1
+
+    def test_edit_item_that_exists(self):
+        self.items.insert({'num': 1, 'page': 1, 'text': 'Bring me Big Bertha!', 'value': '43 points'})
+        response = self.app.post('/items/1', data=json.dumps(dict(page=2)), content_type="application/json")
+        
+        assert response.status_code == 200
+
+        item = self.items.find_one({'text': 'Bring me Big Bertha!'})
+        assert item['page'] == 2
+
+    def test_edit_item_that_does_not_exist(self):
+        self.items.insert({'num': 1, 'page': 1, 'text': 'Bring me Big Bertha!', 'value': '43 points'})
+        response = self.app.post('/items/2', data=json.dumps(dict(page=2)), content_type="application/json")
+        
+        assert response.status_code == 404
+
+    def test_delete_item_that_exists(self):
+        self.items.insert({'num': 1, 'page': 1, 'text': 'Bring me Big Bertha!', 'value': '43 points'})
+        response = self.app.delete('/items/1')
+
+        assert response.status_code == 200
+
+    def test_delete_nonexisting_item(self):
+        self.items.insert({'num': 1, 'page': 1, 'text': 'Bring me Big Bertha!', 'value': '43 points'})
+        response = self.app.delete('/items/2')
+
+        assert response.status_code == 404
+
+if __name__ == "__main__":
+    unittest.main()
