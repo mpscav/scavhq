19d622fd7318ccb7ee0f1f82ab48adbd29bbaf08
Roberto Pedroso <roberto@rpedroso.com>
2014-03-09 00:38:05 -0600

    angularjs routing

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,5 +1,23 @@
+ScavHQ.controller('NavCtrl', function($scope, $location) {
+    $scope.navLinks = ['items', 'people', 'missions', 'contact'];
+
+    $scope.navClass = function(page) {
+        var currentRoute = $location.path().substring(1);
+        return page == currentRoute ? 'active' : '';
+    };
+});
+
 ScavHQ.controller('ItemsCtrl', function($scope, $http) {
     $http.get('api/items').success(function(data) {
         $scope.items = data;
     });
+});
+
+ScavHQ.controller('PeopleCtrl', function($scope) {
+});
+
+ScavHQ.controller('MissionsCtrl', function($scope) {
+});
+
+ScavHQ.controller('ContactCtrl', function($scope) {
 });
